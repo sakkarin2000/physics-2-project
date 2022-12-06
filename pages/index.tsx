4ad4d445e0e720c68095631f55f9a41c792a9b11
sakkarin2000 <sakkarin.kmutt@mail.kmutt.ac.th>
2022-12-06 22:53:13 +0700

    change to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,12 @@ export default function Home() {
   return (
     <div>
       <div className="flex flex-col h-screen justify-between">
+        <header className="h-5 bg-red-700 flex justify-center ">
+          <a className="text-white text-sm">Sparky Team</a>
+        </header>
         <div className="mb-auto h-10">
           <Parallel />
         </div>
-
-        <footer className="h-5 bg-red-700 flex justify-center ">
-          <a className="text-white text-sm">Sparky Team</a>
-        </footer>
       </div>
     </div>
   );
