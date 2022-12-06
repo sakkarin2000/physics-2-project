4436483c53de4f2b57c7d845fec664cbd5043026
sakkarin2000 <sakkarin.kmutt@mail.kmutt.ac.th>
2022-12-06 22:37:11 +0700

    complete Physics Project

--- pages/Parallel.tsx
+++ pages/Parallel.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 
 import React, { useEffect, useState } from "react";
 export default function Parallel() {
-  const [lb_watt, setlb_watt] = useState(1);
+  const [lb_watt, setlb_watt] = useState(25);
   const [lb_num, setlb_num] = useState(1);
-  const [b_volt, setb_volt] = useState(0);
-  const [r_lightbulb, setr_lightbulb] = useState(1);
+  const [b_volt, setb_volt] = useState(16);
+  const [r_lightbulb, setr_lightbulb] = useState(9.5);
   const [percentLightness, setPercentLightness] = useState(0);
   const [fields, setField] = useState<JSX.Element[]>([]);
   useEffect(() => {
@@ -71,29 +71,33 @@ export default function Parallel() {
           </div>
           <div className="flex justify-center mt-4">
             <div>
-              <a> Formula: I = V/R</a>
+              <a>Using Ohm's Law</a>
               <br></br>
-              <a> Formula: P = V * I </a>
+              <a> Current Formula : I = V/R</a>
+              <br></br>
+              <a> Power Formula: P = V * I </a>
             </div>
           </div>
           <div className="flex justify-center mt-4">
             <div className="w-fit">
               <div className="flex gap-2">
-                <a className="w-full">Battery Voltage</a>
+                <a className="w-full mt-2">Battery Voltage</a>
                 <input
                   type="number"
                   onChange={(e) => {
                     setb_volt(parseFloat(e.target.value));
                   }}
+                  defaultValue={b_volt}
                   className="mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="username"
                   placeholder="Battery-Voltage"
                 ></input>
               </div>
               <div className="flex gap-2">
-                <a className="w-full">Lightbulb&apos;s Watt</a>
+                <a className="w-full mt-2">Lightbulb&apos;s Watt</a>
                 <input
                   type="number"
+                  defaultValue={lb_watt}
                   onChange={(e) => {
                     setlb_watt(parseFloat(e.target.value));
                   }}
@@ -103,9 +107,11 @@ export default function Parallel() {
                 ></input>
               </div>
               <div className="flex gap-2">
-                <a className="w-full">Lightbulb Resistance</a>
+                <a className="w-full mt-2">Lightbulb Resistance</a>
                 <input
                   type="number"
+                  defaultValue={r_lightbulb}
+                  disabled={true}
                   onChange={(e) => {
                     setr_lightbulb(parseFloat(e.target.value));
                   }}
@@ -115,14 +121,17 @@ export default function Parallel() {
                 ></input>
               </div>
               <div className="flex gap-2">
-                <a className="w-full">Number of Lightbulbs</a>
+                <a className="w-full mt-2">Number of Lightbulbs</a>
                 <input
                   type="number"
                   onChange={(e) => {
-                    setlb_num(parseFloat(e.target.value));
+                    if (parseInt(e.target.value) <= 50) {
+                      setlb_num(parseFloat(e.target.value));
+                    }
                   }}
                   className="mt-1 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="username"
+                  defaultValue={lb_num}
                   placeholder="Number of lightbulbs"
                 ></input>
               </div>
@@ -131,12 +140,24 @@ export default function Parallel() {
           <div className="flex justify-center mt-4">
             <div>
               <a>
-                Total Current in Circuit: {b_volt / (r_lightbulb * lb_num)} A
+                Total Current in Circuit:{" "}
+                {(b_volt / (r_lightbulb * lb_num)).toFixed(3)} A
+              </a>
+              <br></br>
+              <a className="text-gray-500">
+                I = V/R ={" "}
+                {b_volt + "/" + "(" + r_lightbulb + "*" + lb_num + ")"} A
               </a>
               <br></br>
               <a>
                 Total Power Consumed in Circuit:{" "}
                 {(b_volt * (b_volt / (r_lightbulb * lb_num))).toFixed(3)} Watt
+              </a>
+              <br></br>
+              <a className="text-gray-500">
+                P = V * I ={" "}
+                {b_volt + "*" + (b_volt / (r_lightbulb * lb_num)).toFixed(3)}{" "}
+                Watt
               </a>
               <br></br>
               <a>
@@ -147,8 +168,23 @@ export default function Parallel() {
           </div>
         </div>
       </div>
-      <div className="flex justify-center">
-        <div className="mt-10">{fields}</div>
+      <div>
+        <div className="absolute ml-[167px] mt-[55px] z-40 text-white bg-red-600 p-1 rounded-sm px-3">
+          {b_volt + " V"}
+        </div>
+        <div className="absolute ml-[144px] z-30 ">
+          <Image
+            src="/car-battery.png"
+            alt="Battery img"
+            width={100}
+            height={100}
+            // blurDataURL="data:..." automatically provided
+            // placeholder="blur" // Optional blur-up while loading
+          />
+        </div>
+        <div className="flex justify-center mt-[35px]">
+          <div className="mt-10">{fields}</div>
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,14 @@ import Parallel from "./Parallel";
 export default function Home() {
   return (
     <div>
-      <div className="">
-        <Parallel />
+      <div className="flex flex-col h-screen justify-between">
+        <div className="mb-auto h-10">
+          <Parallel />
+        </div>
+
+        <footer className="h-5 bg-red-700 flex justify-center ">
+          <a className="text-white text-sm">Sparky Team</a>
+        </footer>
       </div>
     </div>
   );
